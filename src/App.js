a255abcf9eed5274a25ee783ef5e61bc5f9ada8d
dipak <dipaksamantaray2@gmail.com>
2022-03-12 15:50:47 +0530

    initialize

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Digital from "./digital"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Digital/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/digital.jsx
@@ -0,0 +1,17 @@
+import React,{useState} from "react";
+ const Digital =()=>{
+     let time =new Date().toLocaleTimeString();
+     const[ctime,setCtime] =useState(time);
+     const UpdateTime=()=>{
+         time = new Date().toLocaleTimeString();
+         setCtime(time);
+     };
+     setInterval(UpdateTime,1000);
+     return(
+         <div className="digi">
+
+         <h1>{ctime}</h1>
+         </div>
+     );
+ };
+ export default Digital;
